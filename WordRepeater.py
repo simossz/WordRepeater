112a01e140e90df847ae7e7b686c621194534c79
Simo <simoisproductive@gmail.com>
2026-03-24 17:01:38 +0100

    first commit

---
+++ WordRepeater.py
@@ -0,0 +1,9 @@
+# Zani Simone
+# 24/03/2026
+parola = input("Scrivi una parola.")
+numero = input("Scrivi quante volte vuoi vederla.")
+
+risultato = (parola + " ") * int(numero)
+# " " Così se parola = Simo non è SimoSimoSimo ma è Simo Simo Simo
+
+print(risultato)
